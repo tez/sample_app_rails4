5c154f386e55d8ce03c1efd1ce2aa1db87be4025
tez <ns7tetsuya@gmail.com>
2013-08-14 20:29:10 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,10 +15,19 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-  describe "when name is not present" do
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+     describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
